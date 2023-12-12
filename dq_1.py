4c45ec96bb64f33ea397b133a606a3d1b333fefc
Dmitri Gornakov <helikeel@gmail.com>
2023-12-12 22:09:03 +0200

    update: bag fixed

--- dq_1.py
+++ dq_1.py
@@ -4,18 +4,8 @@ from pymongo import MongoClient
 
 # bot = BOT_TOKEN
 bot = telebot.TeleBot("6704490108:AAGscrT9P2AadTN20eH7vRNcjPqL8WNVNtE")
-# NEW API 6704490108:AAGscrT9P2AadTN20eH7vRNcjPqL8WNVNtE
-# NEVER USE Old API, it's online NOW!!! 5622719505:AAF5EWHGxI6dbkQqRu4JMwgNdIsoR6IuCTY
 
-# @bot.message_handler(commands=['start', 'help'])
-# def send_welcome(message):
-#     welcome_message = "Привет! Я - ваш бот. Я готов помочь вам [вставьте описание функционала бота]."
-#     bot.reply_to(message, welcome_message)
 
-# @bot.message_handler(func=lambda message: True)
-# def send_welcome(message):
-#     welcome_message = "Привет! Я - ваш бот. Я готов помочь вам [вставьте описание функционала бота]."
-#     bot.reply_to(message, welcome_message)
 
 class DataBase:
 	def __init__(self):
@@ -45,16 +35,17 @@ class DataBase:
 			"chat_id": chat_id,
 			"is_passing": False,
 			"is_passed": False,
-			# "paid": False,
+			"paid": False,
 			"question_index": None,
 			"answers": [],
-			# "course_index": None,
-			# "course_answers": []
+			"course_index": None,
+			"course_answers": []
 		}
 
 		self.users.insert_one(user)
 
 		return user
+
 
 	def set_user(self, chat_id, update):
 		self.users.update_one({"chat_id": chat_id}, {"$set": update})
@@ -67,19 +58,28 @@ db = DataBase()
 
 @bot.message_handler(commands=["start"])
 def start(message):
+	# bot.send_message(message.from_user.id, "Начало работы")
+	print("Начало работы")
+
 	user = db.get_user(message.chat.id)
+	# print("Создание нового пользователя")
 
 	if user["is_passed"]:
+		print("Если уэе прошел")
 		bot.send_message(message.from_user.id, "Вы прошли бесплатную пробную версию курса по джаз-вокалу. Для получения доступа к полной версии необходимо оформить единоразовую подписку 👍😇 для этого переведите 30 евро на счет Eugenia Evy Anstal-Põld EE282200001109459014 и укажите в пояснении свой номер телефона")
 		return
 
 	if user["is_passing"]:
+		print("Начало прохождения курса")
 		return
 
-	db.set_user(message.chat.id, {"question_index": 0, "is_passing": True})
 
+	db.set_user(message.chat.id, {"question_index": 0, "is_passing": True})
+	# print("установили пользователя")
 	user = db.get_user(message.chat.id)
+	# print("Получили пользователя")
 	post = get_question_message(user)
+	# print("Получили вопрос")
 	if post is not None:
 		bot.send_message(message.from_user.id, post["text"], reply_markup=post["keyboard"])
 
@@ -113,18 +113,18 @@ def next(query):
 		bot.edit_message_text(post["text"], query.message.chat.id, query.message.id,
 						 reply_markup=post["keyboard"])
 
-# Написать новый Callback	для перехода к боту оплаты
-@bot.callback_query_handler(func=lambda query: query.data == "?pay")
-def pay(query):
-	user = db.get_user(query.message.chat.id)
-
-	# Здесь вы можете сгенерировать ссылку на бот №2 с параметрами, если это необходимо
-	# payments_bot = "https://web.telegram.org/k/#@JazzVocalClassesBot"
-	payments_bot = "https://web.telegram.org/k/#@msnsgerBot"
-	# bot.send_message(message.from_user.id, f"Для оплаты перейдите по ссылке: {payments_bot}")
-	bot.send_message(query.from_user.id, f"Для оплаты перейдите по ссылке: {payments_bot}")
-	# bot.send_message(message.from_user.id, f"Для оплаты перейдите по ссылке: {payments_bot}")
-	# bot.send_message(query.from_user.id, "Вы прошли бесплатную пробную версию курса по джаз-вокалу. Для получения доступа к полной версии необходимо оформить единоразовую подписку 👍😇 для этого переведите 30 евро на счет Eugenia Evy Anstal-Põld EE282200001109459014 и укажите в пояснении свой номер телефона")
+# # Написать новый Callback	для перехода к боту оплаты
+# @bot.callback_query_handler(func=lambda query: query.data == "?pay")
+# def pay(query):
+# 	user = db.get_user(query.message.chat.id)
+#
+# 	# Здесь вы можете сгенерировать ссылку на бот №2 с параметрами, если это необходимо
+# 	# payments_bot = "https://web.telegram.org/k/#@JazzVocalClassesBot"
+# 	payments_bot = "https://web.telegram.org/k/#@msnsgerBot"
+# 	# bot.send_message(message.from_user.id, f"Для оплаты перейдите по ссылке: {payments_bot}")
+# 	bot.send_message(query.from_user.id, f"Для оплаты перейдите по ссылке: {payments_bot}")
+# 	# bot.send_message(message.from_user.id, f"Для оплаты перейдите по ссылке: {payments_bot}")
+# 	# bot.send_message(query.from_user.id, "Вы прошли бесплатную пробную версию курса по джаз-вокалу. Для получения доступа к полной версии необходимо оформить единоразовую подписку 👍😇 для этого переведите 30 евро на счет Eugenia Evy Anstal-Põld EE282200001109459014 и укажите в пояснении свой номер телефона")
 
 def get_question_message(user):
 	if user["question_index"] == db.questions_count:
@@ -147,14 +147,14 @@ def get_question_message(user):
 
 		text = f"Вы прошли бесплатную пробную версию курса по джаз-вокалу. Для получения доступа к полной версии необходимо оформить единоразовую подписку 👍😇 для этого переведите 30 евро на счет Eugenia Evy Anstal-Põld EE282200001109459014 и укажите в пояснении свой номер телефона"
 		# Встраивание новой кнопки "К оплате" >> функция запуска нового бота
-		keyboard2 = telebot.types.InlineKeyboardMarkup()
-		keyboard2.row(telebot.types.InlineKeyboardButton("Оплатить Курс 'Джаз-Вокал'", callback_data="?pay"))
+		# keyboard2 = telebot.types.InlineKeyboardMarkup()
+		# keyboard2.row(telebot.types.InlineKeyboardButton("Оплатить Курс 'Джаз-Вокал'", callback_data="?pay"))
 
 		db.set_user(user["chat_id"], {"is_passed": False, "is_passing": False})
 
 		return {
 			"text": text,
-			"keyboard": keyboard2
+			"keyboard": None
 		}
 
 	question = db.get_question(user["question_index"])
@@ -173,6 +173,7 @@ def get_question_message(user):
 		"text": text,
 		"keyboard": keyboard
 	}
+
 
 def get_answered_message(user):
 	question = db.get_question(user["question_index"])
